8910a30f0ed98f890b308ec8ba502894b5215aac
wongerchris <wongerchris@gmail.com>
2016-08-12 11:57:05 -0700

    initial commit

---
+++ themes/inhabitent-theme/front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+				<h1>Hewwwo Wooold</h1>
+			
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ themes/inhabitent-theme/inc/extras.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates.
+ *
+ * @package RED_Starter_Theme
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function red_starter_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'red_starter_body_classes' );
+
+
+// Remove "Editor" links from sub-menus
+function inhabitent_remove_submenus() {
+    remove_submenu_page( 'themes.php', 'theme-editor.php' );
+    remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+}
+add_action( 'admin_menu', 'inhabitent_remove_submenus', 110 );
+
+//------
+function my_login_logo() { ?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_template_directory_uri(); ?>/images/inhabitent-logo-text-dark.svg);
+            padding-bottom: 30px;
+            background-size: 300px 53px;
+            height:53px;
+            width:300px;
+        }
+    </style>
+<?php }
+add_action( 'login_head', 'my_login_logo' );
+
+
+//------
+function inhabitent_login_url(){
+	return home_url();
+}
+
+add_filter('login_headerurl','inhabitent_login_url');
+
+//------
+function inhabitent_title_hover(){
+	return 'Inhabitent Supply Co';
+}
+
+add_filter('login_headertitle','inhabitent_title_hover');
+
+
+
+
