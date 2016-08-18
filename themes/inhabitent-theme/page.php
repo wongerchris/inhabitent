3d2f32f7f5477a415282e151e211393f79b962ad
wongerchris <wongerchris@gmail.com>
2016-08-18 16:24:49 -0700

    layout for contact page

--- themes/inhabitent-theme/page.php
+++ themes/inhabitent-theme/page.php
@@ -9,15 +9,23 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+		
+		<div class="page-wrapper container">
+			<div class="page-box">
+				<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php endwhile; // End of the loop. ?>
+			</div>
+			<div class="widget-sidebar">
+				<?php get_sidebar(); ?>
+			</div>
 
-			<?php endwhile; // End of the loop. ?>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
