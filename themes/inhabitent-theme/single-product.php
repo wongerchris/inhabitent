564bfb7b49f42c274c45249ea9ce61b23f93c5b8
wongerchris <wongerchris@gmail.com>
2016-08-19 10:55:02 -0700

    styling for taxonomy terms page

--- themes/inhabitent-theme/single-product.php
+++ themes/inhabitent-theme/single-product.php
@@ -10,6 +10,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<div class="single-product-wrapper container">
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -17,23 +18,19 @@ get_header(); ?>
 					<?php if ( has_post_thumbnail() ) : ?>
 						<?php the_post_thumbnail( 'large' ); ?>
 					<?php endif; ?>
-
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 					<?php echo CFS()->get( 'product_price' ); ?>
 					<?php echo CFS()->get( 'product_information' ); ?>
 					
 					<?php the_content(); ?>
 		
 				</div><!-- .entry-content -->
-			</article><!-- #post-## -->
-
-			
+			</article><!-- #post-## -->			
 		<?php endwhile; // End of the loop. ?>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
---
+++ themes/inhabitent-theme/taxonomy.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying all product entries.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		
+		
+		<header class="shop-nav container">
+			<?php 
+				$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ); 
+					echo '<h2>'.$term->name.'</h2>'; 
+					echo '<p class="term-description">'.$term ->description.'</p>'
+			?>
+
+		</header>
+
+		<section class="shop-wrapper container">
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'shop' ); ?>
+			
+		<?php endwhile; // End of the loop. ?>
+		
+		</section>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
